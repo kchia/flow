f67be13cd8e870a0151c7a4bddaf89cb23ce0b9e
kchia <kchia87@gmail.com>
2015-03-11 16:07:04 -0700

    CLEANUP cleans up code

--- server/server.js
+++ server/server.js
@@ -3,12 +3,12 @@ var mongoose    = require('mongoose');
 
 var app = express();
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 8000;
 
 app.listen(port);
+
 mongoose.connect('mongodb://localhost/flow'); 
 
 require('./config/middleware.js')(app, express);
 
 module.exports = app;
-
